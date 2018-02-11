843870482ff9c5b4851755500a68dac43a5c6976
mohammad kazazi <mohammad.kazazi@gmail.com>
2018-02-11 10:15:41 -0800

    add export database + import database + export excel

--- app/src/main/java/com/samsungfaults/zilu/samsungfaults/MainActivity.java
+++ app/src/main/java/com/samsungfaults/zilu/samsungfaults/MainActivity.java
@@ -2,6 +2,7 @@ package com.samsungfaults.zilu.samsungfaults;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Environment;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -15,17 +16,28 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.channels.FileChannel;
 import java.util.List;
 
 import saman.zamani.persiandate.PersianDate;
 import saman.zamani.persiandate.PersianDateFormat;
+import com.ajts.androidmads.library.SQLiteToExcel;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener, AddReportDialog.Adding {
 
     private TextView persianDate;
     private RecyclerView rvReport;
+    private File dbFile;
+    private DatabaseHelper databaseHelper;
+    private SQLiteToExcel sqliteToExcel;
+    private FileUtils fileUtils;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,7 +49,7 @@ public class MainActivity extends AppCompatActivity
         persianDate = (TextView) findViewById(R.id.persianDate);
         PersianDate pdate = new PersianDate();
         PersianDateFormat pdformater1 = new PersianDateFormat("Y/m/d");
-//        pdformater1.format(pdate);
+
         persianDate.setText(persianDate.getText() + pdformater1.format(pdate));
 
         rvReport = (RecyclerView) findViewById(R.id.rvReport);
@@ -136,7 +148,14 @@ public class MainActivity extends AppCompatActivity
         } else if (id == R.id.nav_stationFaults) {
             Intent inent = new Intent(this, StationFaultsActivity.class);
             startActivity(inent);
+        } else if (id == R.id.nav_exportExcel) {
+            exportToExcel();
+        } else if (id == R.id.nav_exportDb) {
+            exportDB();
+        } else if (id == R.id.nav_importDb) {
+            importDatabase();
         }
+
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
@@ -146,5 +165,104 @@ public class MainActivity extends AppCompatActivity
     @Override
     public void finishAdding() {
         getReport();
+    }
+
+    //exporting database
+    public void exportDB() {
+        // TODO Auto-generated method stub
+
+        try {
+            File sd = Environment.getExternalStorageDirectory();
+            File data = Environment.getDataDirectory();
+
+            if (sd.canWrite()) {
+                String  currentDBPath= "//data//" + "com.samsungfaults.zilu.samsungfaults"
+                        + "//databases//" + "SamsungFaults.db";
+                String directory_path = Environment.getExternalStorageDirectory().getPath() + "/Techwin_SAMSUNG/";
+                File file = new File(directory_path);
+                if (!file.exists()) {
+                    file.mkdirs();
+                }
+
+                String backupDBPath  =  "/Techwin_SAMSUNG/SamsungFaults.db";
+                File currentDB = new File(data, currentDBPath);
+                File backupDB = new File(sd, backupDBPath);
+
+                FileChannel src = new FileInputStream(currentDB).getChannel();
+                FileChannel dst = new FileOutputStream(backupDB).getChannel();
+                dst.transferFrom(src, 0, src.size());
+                src.close();
+                dst.close();
+                Toast.makeText(getBaseContext(), "دیتابیس با موفقیت استخراج شد",
+                        Toast.LENGTH_LONG).show();
+
+            }
+        } catch (Exception e) {
+
+            Toast.makeText(getBaseContext(), e.toString(), Toast.LENGTH_LONG)
+                    .show();
+
+        }
+    }
+
+    public void importDatabase() {
+        String DB_FILEPATH = "/data/data/com.samsungfaults.zilu.samsungfaults/databases/SamsungFaults.db";
+        String directory_path = Environment.getExternalStorageDirectory().getPath() + "/Techwin_SAMSUNG/";
+        File file = new File(directory_path);
+        if (!file.exists()) {
+            file.mkdirs();
+            return;
+        }
+
+        // Close the SQLiteOpenHelper so it will commit the created empty
+        // database to internal storage.
+        // databaseHelper.close();
+        File newDb = new File(directory_path + "SamsungFaults.db");
+        File oldDb = new File(DB_FILEPATH);
+        if (newDb.exists()) {
+            try {
+                fileUtils.copyFile(new FileInputStream(newDb), new FileOutputStream(oldDb));
+            } catch (IOException e) {
+                Toast.makeText(getBaseContext(), "خطا در ورود دیتابیس",
+                        Toast.LENGTH_LONG).show();
+                e.printStackTrace();
+            }
+            // Access the copied database so SQLiteHelper will cache it and mark
+            // it as created.
+            // databaseHelper.getWritableDatabase().close();
+            return ;
+        }
+        return ;
+    }
+
+    public void  exportToExcel() {
+        String directory_path = Environment.getExternalStorageDirectory().getPath() + "/Techwin_SAMSUNG/";
+        File file = new File(directory_path);
+        if (!file.exists()) {
+            file.mkdirs();
+        }
+
+        PersianDate pdate = new PersianDate();
+        PersianDateFormat pdformater = new PersianDateFormat("Y-m-d");
+        // pdformater.format(pdate);
+        String fileName = "faults_" + pdformater.format(pdate) + ".xls";
+
+        sqliteToExcel = new SQLiteToExcel(this, "SamsungFaults.db", directory_path);
+        sqliteToExcel.exportSingleTable("faults", fileName, new SQLiteToExcel.ExportListener() {
+            @Override
+            public void onStart() {
+
+            }
+            @Override
+            public void onCompleted(String filePath) {
+
+            }
+            @Override
+            public void onError(Exception e) {
+
+            }
+        });
+        Toast.makeText(getBaseContext(), "استخراج انجام شد",
+                Toast.LENGTH_LONG).show();
     }
 }
